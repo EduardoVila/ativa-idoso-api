d3d9e600272f8c77ce3c73a27d6e2b78531e6135
pbbyrro <pbbyrro@gmail.com>
2024-11-07 16:36:53 -0300

    Fixing Time.zone in Idwall factories

--- spec/factories/idwall/trial_parts.rb
+++ spec/factories/idwall/trial_parts.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :idwall_trial_part, class: 'Idwall::TrialPart' do
     cnpj { nil }
     cpf { Faker::CPF.pretty }
-    birth_date { Time.now }
+    birth_date { Time.zone.today }
     name { Faker::Name.name }
     rg { Faker::IdNumber.brazilian_id }
     gender { nil }
