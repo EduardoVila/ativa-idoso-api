cfbe6c926678c9d77e635b972054e5d035ff0225
Eduardo De Vila <eduardodevila@MacBook-Pro-de-Eduardo.local>
2025-02-18 13:41:09 -0300

    fixing clone analysis item

--- app/services/analysis/create_analysis_items_service.rb
+++ app/services/analysis/create_analysis_items_service.rb
@@ -35,7 +35,7 @@ module Analysis
       new_analysis_item.update(
         clone_of: previous_analysis_item,
         report: analysis_report,
-        status: :todo,
+        status: :done,
         prediction: previous_analysis_item.prediction.dup,
         features: previous_analysis_item.features,
         disapproval_situation: previous_analysis_item.disapproval_situation
