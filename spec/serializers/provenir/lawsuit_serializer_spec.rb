271e87847480233276f7571bb314b65aea99566b
Eugênio Mesquita Higgins Azevedo dos Santos <eugenio.santos@alpop.com.br>
2025-03-27 13:28:12 -0300

    fixing spec

--- spec/serializers/provenir/lawsuit_serializer_spec.rb
+++ spec/serializers/provenir/lawsuit_serializer_spec.rb
@@ -60,42 +60,10 @@ RSpec.describe Provenir::LawsuitSerializer do
   it { is_expected.to serialize_attribute(:id).from(lawsuit) }
   it { is_expected.to serialize_attribute(:defendant).from(lawsuit) }
   it { is_expected.to serialize_attribute(:disapproved).from(lawsuit) }
-
-  describe 'custom methods' do
-    describe '#uf' do
-      subject { serialized[:uf] }
-
-      it { is_expected.to eq(lawsuit.state) }
-    end
-
-    describe '#trial_number' do
-      subject { serialized[:trial_number] }
-
-      it { is_expected.to eq(lawsuit.lawsuit_number) }
-    end
-
-    describe '#delivery_date' do
-      subject { serialized[:delivery_date] }
-
-      it { is_expected.to eq(lawsuit.notice_date) }
-    end
-
-    describe '#area' do
-      subject { serialized[:area] }
-
-      it { is_expected.to eq(lawsuit.main_subject) }
-    end
-
-    describe '#trial_class_name' do
-      subject { serialized[:trial_class_name] }
-
-      it { is_expected.to eq(lawsuit.lawsuit_type) }
-    end
-
-    describe '#court' do
-      subject { serialized[:court] }
-
-      it { is_expected.to eq(lawsuit.court_name) }
-    end
-  end
+  it { is_expected.to serialize_attribute(:lawsuit_number).from(lawsuit) }
+  it { is_expected.to serialize_attribute(:state).from(lawsuit) }
+  it { is_expected.to serialize_attribute(:court_name).from(lawsuit) }
+  it { is_expected.to serialize_attribute(:notice_date).from(lawsuit) }
+  it { is_expected.to serialize_attribute(:main_subject).from(lawsuit) }
+  it { is_expected.to serialize_attribute(:lawsuit_type).from(lawsuit) }
 end
